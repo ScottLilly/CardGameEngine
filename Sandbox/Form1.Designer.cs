88f8f1330bcd2e06b25106c037f11a378c971847
Scott Lilly <scottlilly@scottlilly.com>
2014-12-28 19:40:54 -0300

    Added a Windows Form "sandbox" project
    I created this project to let me run through a large number of simulated
    hands, and confirm that the random number methods results in the
    expected distribution of poker hands.
    
    It does.

---
+++ Sandbox/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Sandbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.rtbResults = new System.Windows.Forms.RichTextBox();
+            this.txtHandsToDeal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(13, 48);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(83, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // rtbResults
+            // 
+            this.rtbResults.Location = new System.Drawing.Point(114, 13);
+            this.rtbResults.Name = "rtbResults";
+            this.rtbResults.Size = new System.Drawing.Size(335, 301);
+            this.rtbResults.TabIndex = 1;
+            this.rtbResults.Text = "";
+            // 
+            // txtHandsToDeal
+            // 
+            this.txtHandsToDeal.Location = new System.Drawing.Point(13, 13);
+            this.txtHandsToDeal.Name = "txtHandsToDeal";
+            this.txtHandsToDeal.Size = new System.Drawing.Size(83, 20);
+            this.txtHandsToDeal.TabIndex = 2;
+            this.txtHandsToDeal.Text = "1000";
+            this.txtHandsToDeal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 335);
+            this.Controls.Add(this.txtHandsToDeal);
+            this.Controls.Add(this.rtbResults);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.RichTextBox rtbResults;
+        private System.Windows.Forms.TextBox txtHandsToDeal;
+    }
+}
+
---
+++ Sandbox/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using CardGameEngine;
+using CardGameEngine.Hands;
+
+namespace Sandbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Use this to confirm the random shuffler produces the expected hand distribution results.
+
+            rtbResults.Clear();
+
+            SortedList<PokerHandValues, int> handsDealt = new SortedList<PokerHandValues, int>();
+
+            int handsToDeal = Convert.ToInt32(txtHandsToDeal.Text);
+
+            for(int i = 0; i < handsToDeal; i++)
+            {
+                Deck deck = Shuffler.GetShuffledDeck();
+
+                PokerHand hand = new PokerHand("");
+
+                for(int j = 0; j < 5; j++)
+                {
+                    Card card = deck.TakeNextCard();
+                    hand.AddCard(card.Value, card.Suit);
+                }
+
+                if(!handsDealt.ContainsKey(hand.Value))
+                {
+                    handsDealt.Add(hand.Value, 0);
+                }
+
+                handsDealt[hand.Value]++;
+            }
+
+            rtbResults.Clear();
+
+            foreach(KeyValuePair<PokerHandValues, int> keyValuePair in handsDealt)
+            {
+                rtbResults.Text += string.Format("{0}:\t {1}\t {2}",
+                                                 keyValuePair.Key, keyValuePair.Value,
+                                                 (Convert.ToDouble(keyValuePair.Value) / Convert.ToDouble(handsToDeal) * 100));
+                rtbResults.Text += Environment.NewLine;
+            }
+        }
+    }
+}
